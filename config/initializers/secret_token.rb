837eda6f1951e779029fc77118d42abde7feef83
Charlie Sahlman <csahlman@gmail.com>
2013-02-01 12:40:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Failboat::Application.config.secret_token = '00fde013fdb8bf7c2d5375d21bb3322d209d34eabb15f8cd9a22b59734a2b2843058907ebeecf578529f0c46f010977fca58b690a9cd580ff2493637b57fc06b'
