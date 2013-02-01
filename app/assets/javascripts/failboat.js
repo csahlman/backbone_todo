d18d61645c481a9a649c1b2336f867525aeb18bf
Charlie Sahlman <csahlman@gmail.com>
2013-02-01 12:41:37 -0500

    --javascript

---
+++ app/assets/javascripts/failboat.js
@@ -0,0 +1,13 @@
+window.Failboat = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Failboat.initialize();
+});
